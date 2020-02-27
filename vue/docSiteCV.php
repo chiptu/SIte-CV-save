4de8f93ff177f70e13f5d1fe4ab6b8cebfc9f09f
alex <alexle-guillou@outlook.fr>
2020-02-27 15:52:29 +0100

    docSiteCV SEO

--- vue/docSiteCV.php
+++ vue/docSiteCV.php
@@ -76,23 +76,34 @@
 				<p>J'ai codé ce site en utilisant pour seul librairie/framework bootstrap qui permet d'avoir des éléments responsiv (les éléments s'adaptent au format pc/tablette/mobile).
 				 BootStrap inclut Jquery qui est une librairie de JavaScript permettant de rendre des éléments html dynamiques (boutons , collapses...).</p>
 				 <p>Comme vous pouvez le voir ci-dessous l'arborescence du site est au format MVC (Modèle Vue Contrôleur) et le html est renvoyé par du php
-				 alors que je n'utilise pas de modèle/contrôleur. En réalité c'est au cas où pour l'évolutivité du site et aussi je dois l'avouer que dans ma formation c'était devenu une habitude de commencer les projets comme cela.</p><br>
+				 alors que je n'utilise pas de modèle/contrôleur. En réalité c'est au cas où pour l'évolutivité du site et aussi je dois l'avouer que dans ma formation c'était devenu une habitude de commencer les projets comme cela.</p>
 			
-				<img class="picture h-25 "
+				<img class="picture h-5 m-4"
     			 src="../images/doc/ImgSiteCV/arbo.png"
-     			 alt="Arborescence code SiteCV">
+     			 alt="Arborescence code SiteCV"><br>
 			
 			
 			<h2 id ="1">BootStrap/Jquery</h2><br>
 				<p> Bootstrap est un framework/librairie de html/css et inclut même certains éléments de jquery (librairie en js pour dynamiser le html)
 				 comme des boutons pop up / collapses ....
-				 Bootstrap est initialement un outil interne à twitter qui est passé en open source en 2011 et qui est devenue le projet le plus populaire de Github en 2012.</p><br>
+				 Bootstrap est initialement un outil interne à twitter qui est passé en open source en 2011 
+				 et qui est devenue le projet le plus populaire de Github en 2012.</p><br>
 			
 				<h4 id ="1-1">Base de bootstrap</h4><br>
 				<h4 id ="1-1">Exemples d'éléments</h4><br>
 			
 			<h2 id ="2">SEO</h2><br>
+				<p>	SEO est l'acronyme de Search Engine Optimization ce qui correspond à l'optimisation pour les moteurs de recherche.
+				Cette optimisation est un ensemble de critère sur lesquel (Google, DuckDuckGo, Qwant, Yahoo...) vont indexer les sites en fonction
+				de la recherche de l'utilisateur. </p><br>
+
 				<h4 id ="1-1">Les critères</h4><br>
+
+				<div class ="ph4"> Il existe de nombreux sites qui listent les critères d'optimisations pour les moteurs de recherche j'en ai comparé 
+				plusieurs et woorank me semble le plus claire, vous pouvez retrouvez l'analyse de mon site sur woorank en cliquant
+				<a id ="lien"class="text-primary d-inline ml-0" href="https://www.woorank.com/fr/www/leguilloualexandre.fr">ici</a>.</div>
+				
+
 				<h4 id ="1-1">Exemple de mon site</h4><br>
 			
 			<h2 id ="3">Hébergement</h2><br>
